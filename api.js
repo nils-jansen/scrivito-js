b16c6f1f95f3e709b982485a38960428d8a23e27
Nils Jansen <nils@fabbits.de>
2021-02-02 20:29:40 +0100

    Packaged?

--- api.js
+++ api.js
@@ -1,103 +1,190 @@
 const axios = require("axios").default;
-class Scrivito {
-	#tenantID;
-	#workspaceID;
-	#apiKey;
 
-	constructor(tenantID, apiKey, workspaceID = "published") {
-		if (tenantID && apiKey) {
-			this.#tenantID = tenantID;
-			this.#apiKey = apiKey;
-			this.#workspaceID = workspaceID;
-		} else {
-			throw new Error("Tenant ID and API Key are required.");
+/**
+ * Returns a Promise which resolves to the requested object.
+ *
+ * @param {String} id - The ID of the object to get
+ * @param {String} workspace - ID of the workspace to get the object from, "published" by default.
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function getObject(id, workspace = "published", tenant, apiKey) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let response = await axios.get(
+				`https://api.scrivito.com/tenants/${tenant}/workspaces/${workspace}/objs/${id}`,
+				{
+					auth: {
+						username: "api_token",
+						password: apiKey,
+					},
+				}
+			);
+			if (Math.floor(response.status / 100) == 2) {
+				resolve(response.data);
+			} else {
+				reject(response);
+			}
+		} catch (error) {
+			reject(error.response.data.code || error);
 		}
-	}
+	});
+}
 
-	/**
-	 * @returns currently set workspace
-	 */
-	getWorkspace() {
-		return this.#workspaceID;
-	}
+/**
+ * Returns a promise which resolves if the Object is deleted successfully.
+ *
+ * @param {String} id - The ID of the object to be deleted
+ * @param {String} workspace - ID of the workspace to delete the object from, "published" by default.
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function deleteObject(id, workspace = "published", tenant, apiKey) {
+	return new Promise(async (resolve, reject) => {
+		if (workspace == "published") {
+			reject('Workspace can\'t be "published" when deleting an object.');
+		}
+		try {
+			let response = await axios.delete(
+				`https://api.scrivito.com/tenants/${tenant}/workspaces/${workspace}/objs/${id}`,
+				{
+					auth: {
+						username: "api_token",
+						password: apiKey,
+					},
+				}
+			);
+			if (Math.floor(response.status / 100) == 2) {
+				// TODO: Test if success response is actually 2xx code
+				resolve();
+			} else {
+				reject(response);
+			}
+		} catch (error) {
+			reject(error.response.data.code || error);
+		}
+	});
+}
 
-	/**
-	 * @returns currently set tenant ID
-	 */
-	getTenant() {
-		return this.#tenantID;
+/**
+ * Returns a promise which resolves if the Object is updated successfully.
+ * The id is extracted from the Object parameter.
+ *
+ * @param {Object} obj - Object to update. Needs to have a "_id" property
+ * @param {String} workspace - ID of the workspace to update the object in, must not be left blank.
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function updateObject(obj, workspace = "published", tenant, apiKey) {
+	if (workspace == "published") {
+		reject('Workspace can\'t be "published" when updating an object.');
 	}
-
-	/**
-	 *
-	 * @param {String} workspaceID - ID of the workspace to use for following operations
-	 */
-	setWorkspace(workspaceID) {
-		this.#workspaceID = workspaceID;
-	}
-
-	/**
-	 * Sets the Tenant/API Key to new credentials.
-	 *
-	 * @param {String} tenantID - Scrivito Tenant ID
-	 * @param {String} apiKey - Scrivito API Key / Token
-	 */
-	setTenant(tenantID, apiKey) {
-		this.#tenantID = tenantID;
-		this.#apiKey = apiKey;
-	}
-
-	/**
-	 * Returns a Promise which resolves to the requested object in JSON Format.
-	 *
-	 * @param {String} id - The ID of the object to get
-	 */
-	async getObject(id) {
-		return new Promise(async (resolve, reject) => {
+	return new Promise(async (resolve, reject) => {
+		if (obj && obj._id) {
 			try {
-				let response = await axios.get(
-					`https://api.scrivito.com/tenants/${
-						this.#tenantID
-					}/workspaces/${this.#workspaceID}/objs/${id}`,
+				let response = await axios.post(
+					`https://api.scrivito.com/tenants/${tenant}/workspaces/${workspace}/objs`,
+					obj,
 					{
 						auth: {
 							username: "api_token",
-							password: this.#apiKey,
+							password: apiKey,
 						},
 					}
 				);
 				if (Math.floor(response.status / 100) == 2) {
-					resolve(response.data);
+					// TODO: Test if success response is actually 2xx code
+					resolve(obj);
 				} else {
 					reject(response);
 				}
 			} catch (error) {
-				reject(error.response.data.code || error);
+				reject(error);
 			}
-		});
-	}
+		} else {
+			reject('Object needs to have a "_id" property.');
+		}
+	});
+}
 
-	/**
-	 * Returns a promise which resolves if the Object is deleted successfully.
-	 *
-	 * @param {String} id - The ID of the object to be deleted
-	 */
-	async deleteObject(id) {
-		return new Promise(async (resolve, reject) => {
-			if (this.#workspaceID == "published") {
-				reject(
-					'Workspace can\'t be "published" when deleting an object.'
-				);
-			}
+/**
+ * Returns a promise which resolves to the new workspace ID
+ *
+ * @param {String} title - The title of the workspace to be created
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function createWorkspace(title, tenant, apiKey) {
+	return new Promise(async (resolve, reject) => {
+		if (title) {
 			try {
-				let response = await axios.delete(
-					`https://api.scrivito.com/tenants/${
-						this.#tenantID
-					}/workspaces/${this.#workspaceID}/objs/${id}`,
+				let response = await axios.post(
+					`https://api.scrivito.com/tenants/${tenant}/workspaces`,
+					{
+						workspace: {
+							auto_update: true, // what is this?
+							title: title,
+						},
+					},
 					{
 						auth: {
 							username: "api_token",
-							password: this.#apiKey,
+							password: apiKey,
+						},
+					}
+				);
+
+				var interval = setInterval(async () => {
+					try {
+						let task = await axios.get(
+							`https://api.scrivito.com/tenants/${tenant}/tasks/${response.data.task.id}`,
+							{
+								auth: {
+									username: "api_token",
+									password: apiKey,
+								},
+							}
+						);
+						if (
+							task.data.status == "open" ||
+							(task.data.status == "success" &&
+								task.data.result.id)
+						) {
+							resolve(task.data.result.id);
+						} else {
+							reject("Failed to create working copy.");
+						}
+						clearInterval(interval);
+					} catch (error) {
+						reject(error);
+						clearInterval(interval);
+					}
+				}, 500);
+			} catch (error) {
+				reject(error);
+			}
+		} else {
+			reject("Workspace title is required.");
+		}
+	});
+}
+
+/**
+ * @param {String} id - ID of the workspace to be published
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function publishWorkspace(id, tenant, apiKey) {
+	return new Promise(async (resolve, reject) => {
+		if (id) {
+			try {
+				let response = await axios.put(
+					`https://api.scrivito.com/tenants/${tenant}/workspaces/${id}/publish`,
+					undefined,
+					{
+						auth: {
+							username: "api_token",
+							password: apiKey,
 						},
 					}
 				);
@@ -108,246 +195,140 @@ class Scrivito {
 					reject(response);
 				}
 			} catch (error) {
-				reject(error.response.data.code || error);
-			}
-		});
-	}
-
-	/**
-	 * Returns a promise which resolves if the Object is updated successfully.
-	 * The id is extracted from the Object parameter.
-	 *
-	 * @param {Object} obj - Object to update. Needs to have a "_id" property
-	 */
-	async updateObject(obj) {
-		if (this.#workspaceID == "published") {
-			reject('Workspace can\'t be "published" when updating an object.');
-		}
-		return new Promise(async (resolve, reject) => {
-			if (obj && obj._id) {
-				try {
-					let response = await axios.post(
-						`https://api.scrivito.com/tenants/${
-							this.#tenantID
-						}/workspaces/${this.#workspaceID}/objs`,
-						obj,
-						{
-							auth: {
-								username: "api_token",
-								password: this.#apiKey,
-							},
-						}
-					);
-					if (Math.floor(response.status / 100) == 2) {
-						// TODO: Test if success response is actually 2xx code
-						resolve(obj);
-					} else {
-						reject(response);
-					}
-				} catch (error) {
-					reject(error);
-				}
-			} else {
-				reject('Object needs to have a "_id" property.');
-			}
-		});
-	}
-
-	/**
-	 * Returns a promise which resolves if the new workspace is successfully created.
-	 *
-	 * @param {String} title - The title of the workspace to be created
-	 */
-	async createWorkspace(title) {
-		return new Promise(async (resolve, reject) => {
-			if (title) {
-				try {
-					let response = await axios.post(
-						`https://api.scrivito.com/tenants/${
-							this.#tenantID
-						}/workspaces`,
-						{
-							workspace: {
-								auto_update: true, // what is this?
-								title: title,
-							},
-						},
-						{
-							auth: {
-								username: "api_token",
-								password: this.#apiKey,
-							},
-						}
-					);
-
-					var interval = setInterval(async () => {
-						try {
-							let task = await axios.get(
-								`https://api.scrivito.com/tenants/${
-									this.#tenantID
-								}/tasks/${response.data.task.id}`,
-								{
-									auth: {
-										username: "api_token",
-										password: this.#apiKey,
-									},
-								}
-							);
-							if (
-								task.data.status == "open" ||
-								(task.data.status == "success" &&
-									task.data.result.id)
-							) {
-								resolve(task.data.result.id);
-							} else {
-								reject("Task failed internally.");
-							}
-							clearInterval(interval);
-						} catch (error) {
-							reject(error);
-							clearInterval(interval);
-						}
-					}, 500);
-				} catch (error) {
-					reject(error);
-				}
-			} else {
-				reject("Workspace title is required.");
-			}
-		});
-	}
-
-	/**
-	 * @param {String} id - ID of the workspace to be published
-	 */
-	async publishWorkspace(id) {
-		if (this.#workspaceID == "published") {
-			reject('Can\'t publish workspace "published".');
-		}
-		return new Promise(async (resolve, reject) => {
-			if (id) {
-				try {
-					let response = await axios.put(
-						`https://api.scrivito.com/tenants/${
-							this.#tenantID
-						}/workspaces/${id}/publish`,
-						undefined,
-						{
-							auth: {
-								username: "api_token",
-								password: this.#apiKey,
-							},
-						}
-					);
-					if (Math.floor(response.status / 100) == 2) {
-						// TODO: Test if success response is actually 2xx code
-						resolve();
-					} else {
-						reject(response);
-					}
-				} catch (error) {
-					reject(error);
-				}
-			} else {
-				reject("Workspace ID is required.");
-			}
-		});
-	}
-
-	/**
-	 *
-	 * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
-	 * @returns An array of IDs
-	 */
-	async getIdsByQuery(query) {
-		return this.#getIdsByQueryHelper(query, undefined);
-	}
-
-	async #getIdsByQueryHelper(query, continuation) {
-		return new Promise(async (resolve, reject) => {
-			if (!query) {
-				reject("No query provided");
-			}
-			try {
-				let response = await axios.post(
-					`https://api.scrivito.com/tenants/${
-						this.#tenantID
-					}/workspaces/${this.#workspaceID}/objs/search`,
-					{
-						query: query,
-						continuation: continuation,
-					},
-					{
-						auth: {
-							username: "api_token",
-							password: this.#apiKey,
-						},
-					}
-				);
-				if (response.data.continuation) {
-					resolve(
-						response.data.results.concat(
-							await this.#getIdsByQueryHelper(
-								query,
-								response.data.continuation
-							)
-						)
-					);
-				} else {
-					resolve(response.data.results);
-				}
-			} catch (error) {
 				reject(error);
 			}
-		});
-	}
-
-	/**
-	 *
-	 * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
-	 * @returns An array of objects
-	 */
-	async getObjectsByQuery(query) {
-		return this.#getObjectsByQueryHelper(query, undefined);
-	}
-
-	async #getObjectsByQueryHelper(query, continuation) {
-		return new Promise(async (resolve, reject) => {
-			if (!query) {
-				reject("No query provided");
-			}
-			try {
-				let response = await axios.post(
-					`https://api.scrivito.com/tenants/${
-						this.#tenantID
-					}/workspaces/${this.#workspaceID}/objs/search`,
-					{
-						query: query,
-						continuation: continuation,
-						include_objs: true,
-					},
-					{
-						auth: {
-							username: "api_token",
-							password: this.#apiKey,
-						},
-					}
-				);
-				if (response.data.continuation) {
-					resolve(
-						response.data.objs.concat(
-							await this.#getObjectsByQueryHelper(
-								query,
-								response.data.continuation
-							)
-						)
-					);
-				} else {
-					resolve(response.data.objs);
-				}
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+		} else {
+			reject("Workspace ID is required.");
+		}
+	});
 }
+
+/**
+ * @returns An array of IDs
+ * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
+ * @param {String} workspace - ID of the workspace to get the objects from, "published" by default.
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function getIdsByQuery(query, workspace = "published", tenant, apiKey) {
+	return getIdsByQueryHelper(query, workspace, tenant, apiKey, undefined);
+}
+
+async function getIdsByQueryHelper(
+	query,
+	workspace,
+	tenant,
+	apiKey,
+	continuation
+) {
+	return new Promise(async (resolve, reject) => {
+		if (!query) {
+			reject("No query provided");
+		}
+		try {
+			let response = await axios.post(
+				`https://api.scrivito.com/tenants/${tenant}/workspaces/${workspace}/objs/search`,
+				{
+					query: query,
+					continuation: continuation,
+				},
+				{
+					auth: {
+						username: "api_token",
+						password: apiKey,
+					},
+				}
+			);
+			if (response.data.continuation) {
+				resolve(
+					response.data.results.concat(
+						await getIdsByQueryHelper(
+							query,
+							workspace,
+							tenant,
+							apiKey,
+							response.data.continuation
+						)
+					)
+				);
+			} else {
+				resolve(response.data.results);
+			}
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+
+/**
+ * @returns An array of objects
+ * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
+ * @param {String} workspace - ID of the workspace to get the objects from, "published" by default.
+ * @param {String} tenant - Scrivito Tenant ID
+ * @param {String} apiKey - Scrivito Tenant Rest API Key
+ */
+async function getObjectsByQuery(
+	query,
+	workspace = "published",
+	tenant,
+	apiKey
+) {
+	return getObjectsByQueryHelper(query, workspace, tenant, apiKey, undefined);
+}
+
+async function getObjectsByQueryHelper(
+	query,
+	workspace,
+	tenant,
+	apiKey,
+	continuation
+) {
+	return new Promise(async (resolve, reject) => {
+		if (!query) {
+			reject("No query provided");
+		}
+		try {
+			let response = await axios.post(
+				`https://api.scrivito.com/tenants/${tenant}/workspaces/${workspace}/objs/search`,
+				{
+					query: query,
+					continuation: continuation,
+					include_objs: true,
+				},
+				{
+					auth: {
+						username: "api_token",
+						password: apiKey,
+					},
+				}
+			);
+			if (response.data.continuation) {
+				resolve(
+					response.data.objs.concat(
+						await getObjectsByQueryHelper(
+							query,
+							workspace,
+							tenant,
+							apiKey,
+							response.data.continuation
+						)
+					)
+				);
+			} else {
+				resolve(response.data.objs);
+			}
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+
+module.exports = {
+	getObject: getObject,
+	deleteObject: deleteObject,
+	updateObject: updateObject,
+	createWorkspace: createWorkspace,
+	publishWorkspace: publishWorkspace,
+	getIdsByQuery: getIdsByQuery,
+	getObjectsByQuery: getObjectsByQuery,
+};
