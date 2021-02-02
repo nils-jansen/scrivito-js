058eb5526e13f797fd54c068efba7bd71c10e51a
Nils Jansen <nils@fabbits.de>
2021-02-02 19:52:48 +0100

    Added tons of methods

--- api.js
+++ api.js
@@ -59,7 +59,6 @@ class Scrivito {
 					`https://api.scrivito.com/tenants/${
 						this.#tenantID
 					}/workspaces/${this.#workspaceID}/objs/${id}`,
-					undefined,
 					{
 						auth: {
 							username: "api_token",
@@ -73,7 +72,7 @@ class Scrivito {
 					reject(response);
 				}
 			} catch (error) {
-				reject(error);
+				reject(error.response.data.code || error);
 			}
 		});
 	}
@@ -85,12 +84,16 @@ class Scrivito {
 	 */
 	async deleteObject(id) {
 		return new Promise(async (resolve, reject) => {
+			if (this.#workspaceID == "published") {
+				reject(
+					'Workspace can\'t be "published" when deleting an object.'
+				);
+			}
 			try {
-				let response = axios.delete(
+				let response = await axios.delete(
 					`https://api.scrivito.com/tenants/${
 						this.#tenantID
 					}/workspaces/${this.#workspaceID}/objs/${id}`,
-					undefined,
 					{
 						auth: {
 							username: "api_token",
@@ -105,7 +108,7 @@ class Scrivito {
 					reject(response);
 				}
 			} catch (error) {
-				reject(error);
+				reject(error.response.data.code || error);
 			}
 		});
 	}
@@ -117,6 +120,9 @@ class Scrivito {
 	 * @param {Object} obj - Object to update. Needs to have a "_id" property
 	 */
 	async updateObject(obj) {
+		if (this.#workspaceID == "published") {
+			reject('Workspace can\'t be "published" when updating an object.');
+		}
 		return new Promise(async (resolve, reject) => {
 			if (obj && obj._id) {
 				try {
@@ -134,7 +140,7 @@ class Scrivito {
 					);
 					if (Math.floor(response.status / 100) == 2) {
 						// TODO: Test if success response is actually 2xx code
-						resolve();
+						resolve(obj);
 					} else {
 						reject(response);
 					}
@@ -161,8 +167,10 @@ class Scrivito {
 							this.#tenantID
 						}/workspaces`,
 						{
-							auto_update: true, // what is this?
-							title: title,
+							workspace: {
+								auto_update: true, // what is this?
+								title: title,
+							},
 						},
 						{
 							auth: {
@@ -171,12 +179,35 @@ class Scrivito {
 							},
 						}
 					);
-					if (Math.floor(response.status / 100) == 2) {
-						// TODO: Test if success response is actually 2xx code
-						resolve();
-					} else {
-						reject(response);
-					}
+
+					var interval = setInterval(async () => {
+						try {
+							let task = await axios.get(
+								`https://api.scrivito.com/tenants/${
+									this.#tenantID
+								}/tasks/${response.data.task.id}`,
+								{
+									auth: {
+										username: "api_token",
+										password: this.#apiKey,
+									},
+								}
+							);
+							if (
+								task.data.status == "open" ||
+								(task.data.status == "success" &&
+									task.data.result.id)
+							) {
+								resolve(task.data.result.id);
+							} else {
+								reject("Task failed internally.");
+							}
+							clearInterval(interval);
+						} catch (error) {
+							reject(error);
+							clearInterval(interval);
+						}
+					}, 500);
 				} catch (error) {
 					reject(error);
 				}
@@ -190,6 +221,9 @@ class Scrivito {
 	 * @param {String} id - ID of the workspace to be published
 	 */
 	async publishWorkspace(id) {
+		if (this.#workspaceID == "published") {
+			reject('Can\'t publish workspace "published".');
+		}
 		return new Promise(async (resolve, reject) => {
 			if (id) {
 				try {
@@ -216,6 +250,103 @@ class Scrivito {
 				}
 			} else {
 				reject("Workspace ID is required.");
+			}
+		});
+	}
+
+	/**
+	 *
+	 * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
+	 * @returns An array of IDs
+	 */
+	async getIdsByQuery(query) {
+		return this.#getIdsByQueryHelper(query, undefined);
+	}
+
+	async #getIdsByQueryHelper(query, continuation) {
+		return new Promise(async (resolve, reject) => {
+			if (!query) {
+				reject("No query provided");
+			}
+			try {
+				let response = await axios.post(
+					`https://api.scrivito.com/tenants/${
+						this.#tenantID
+					}/workspaces/${this.#workspaceID}/objs/search`,
+					{
+						query: query,
+						continuation: continuation,
+					},
+					{
+						auth: {
+							username: "api_token",
+							password: this.#apiKey,
+						},
+					}
+				);
+				if (response.data.continuation) {
+					resolve(
+						response.data.results.concat(
+							await this.#getIdsByQueryHelper(
+								query,
+								response.data.continuation
+							)
+						)
+					);
+				} else {
+					resolve(response.data.results);
+				}
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+
+	/**
+	 *
+	 * @param {Object[]} query - The query is an array of objects: [Scrivito Docs](https://www.scrivito.com/search-objects-ec774706d7cb5434)
+	 * @returns An array of objects
+	 */
+	async getObjectsByQuery(query) {
+		return this.#getObjectsByQueryHelper(query, undefined);
+	}
+
+	async #getObjectsByQueryHelper(query, continuation) {
+		return new Promise(async (resolve, reject) => {
+			if (!query) {
+				reject("No query provided");
+			}
+			try {
+				let response = await axios.post(
+					`https://api.scrivito.com/tenants/${
+						this.#tenantID
+					}/workspaces/${this.#workspaceID}/objs/search`,
+					{
+						query: query,
+						continuation: continuation,
+						include_objs: true,
+					},
+					{
+						auth: {
+							username: "api_token",
+							password: this.#apiKey,
+						},
+					}
+				);
+				if (response.data.continuation) {
+					resolve(
+						response.data.objs.concat(
+							await this.#getObjectsByQueryHelper(
+								query,
+								response.data.continuation
+							)
+						)
+					);
+				} else {
+					resolve(response.data.objs);
+				}
+			} catch (error) {
+				reject(error);
 			}
 		});
 	}
